f987eb225b5b853888771c382b19e5364ab2cc50
Omar Cossio <omar119mce@gmail.com>
2020-12-11 00:28:30 -0500

    added handlebars: main and index, advanced the server.js file to add, delete and modify burger information

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,4 @@
 -- Inserted a set of records into the table
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("Beef", false),
-("Turkey", false),
-("Vegan", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Beef", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Vegan", false);
